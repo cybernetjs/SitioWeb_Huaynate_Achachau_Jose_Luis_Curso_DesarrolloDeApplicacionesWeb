270daaac7941a849bef9d97713ad02c1f56ecaa5
cybernetjs <devlegendjs@gmail.com>
2026-06-01 21:53:49 -0500

    Configurar deploy a GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/SitioWeb_Huaynate_Achachau_Jose_Luis_Curso_DesarrolloDeApplicacionesWeb/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
